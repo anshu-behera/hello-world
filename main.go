ed6f80864489208c4bebfafc7006cd791c7db039
Anshu Behera <anshu.behera.02@gmail.com>
2020-11-02 22:13:00 +0100

    some other commit to check if push triggers a build

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	client := http.Client{}
 	resp, _ := client.Get("https://jsonplaceholder.typicode.com/todos/4")
 	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println(string(body)+ "def")
+	log.Println(string(body)+ "def123")
 }
