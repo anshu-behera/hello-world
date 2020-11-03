c6b51c13897ab7338181391521f403405d1bd1bc
Anshu Behera <anshu.behera.02@gmail.com>
2020-11-03 19:38:57 +0100

    adds a server that prints the message from json place holder

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
+	"time"
 	"io/ioutil"
 	"log"
 	"net/http"
+
 )
 
 func main() {
 	log.Println("help")
 	client := http.Client{}
-	resp, _ := client.Get("https://jsonplaceholder.typicode.com/todos/4")
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println(string(body)+ "def123")
+
+	r := mux.NewRouter()
+	myHandler := func (w http.ResponseWriter, r *http.Request)  {
+		resp, _ := client.Get("https://jsonplaceholder.typicode.com/todos/4")
+		body, _ := ioutil.ReadAll(resp.Body)
+		w.Write(body)
+	}
+	r.HandleFunc("/",myHandler)
+	s := &http.Server{
+		Addr:           ":8085",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
